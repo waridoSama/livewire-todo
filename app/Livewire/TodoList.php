7375bbfa99e76d763c74c1b82ea5105dc463fcdd
waridoSama <walid.elmrabet@gmail.com>
2024-03-11 01:20:23 +0000

    created todolist

--- app/Livewire/TodoList.php
+++ app/Livewire/TodoList.php
@@ -2,12 +2,42 @@
 
 namespace App\Livewire;
 
+use App\Models\Todo;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class TodoList extends Component
 {
+    use WithPagination;
+    #[Rule('required|unique:todos')]
+    public $name;
+    #[Rule('')]
+    public $search;
+
+    public function create()
+    {
+        $validated = $this->validateOnly('name');
+        Todo::create($validated);
+        $this->reset('name');
+        session()->flash('success', 'task created');
+    }
+
+    public function delete($id)
+    {
+        $todo = Todo::findOrFail($id);
+        $todo->delete();
+        session()->flash('deleted', 'Deleted successfully!');
+        $this->redirect('/');
+    }
+    public function checked($id)
+    {
+        $todo=Todo::findOrFail($id);
+        $todo->completed = !$todo->completed;
+        $todo->save();
+    }
     public function render()
     {
-        return view('livewire.todo-list');
+        return view('livewire.todo-list', ['todos' => Todo::latest()->where('name', 'like', "%{$this->search}%")->paginate(2)]);
     }
 }
--- resources/views/livewire/todo-list.blade.php
+++ resources/views/livewire/todo-list.blade.php
@@ -1,3 +1,12 @@
 <div>
-    {{-- Be like water. --}}
+    @include('livewire.components.create-todo-box')
+    @include('livewire.components.search-box')
+    <div id="todos-list">
+        @foreach ($todos as $todo)
+            @include('livewire.components.todo-card')
+        @endforeach
+        <div class="my-2">
+            {{ $todos->links() }}
+        </div>
+    </div>
 </div>
